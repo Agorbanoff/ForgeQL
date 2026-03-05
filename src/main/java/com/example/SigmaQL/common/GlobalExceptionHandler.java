720e690022f1421690d87352cb242abc8a3bc0e8
Agorbanoff <alexandargorbanov@gmail.com>
2026-03-05 20:58:06 +0200

    improved the global exception handler

--- src/main/java/com/example/SigmaQL/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/SigmaQL/common/GlobalExceptionHandler.java
@@ -1,15 +1,34 @@
 package com.example.SigmaQL.common;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<String> handleCustomException(CustomException e) {
         return ResponseEntity
                 .status(e.getStatusCode())
                 .body(e.getMessage());
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> handleDataAccess(DataAccessException e) {
+        e.getMostSpecificCause();
+        String msg = e.getMostSpecificCause().getMessage();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("Database error: " + msg);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAny(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Internal error: " + e.getMessage());
     }
 }
